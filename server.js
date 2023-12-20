dd6ff0dd704e22f43d76956397b0d38f0e7cc571
HeatherRouke <heather214@gmail.com>
2023-12-20 11:47:59 -0500

    oAuth setup, tested and working

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const methodOverride = require('method-override')
 
 require('dotenv').config();
 require('./config/database');
-
+require('./config/passport');
 
 
 var indexRouter = require('./routes/index');
