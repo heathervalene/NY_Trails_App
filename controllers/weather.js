02768e9d66311a035e1b6899d9fbe98463f21c79
HeatherRouke <heather214@gmail.com>
2023-12-30 15:45:36 -0500

    weather controller bug fix

--- controllers/weather.js
+++ controllers/weather.js
@@ -2,15 +2,15 @@ const axios = require('axios');
 
 const apiKey = 'd16785159270c1f96141537f606e54da';
 
-async function getWeather(res,req) {
+async function getWeather(req,res) {
     try {
         const apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=42.01&lon=-74.38&appid=${apiKey}`;
         const response = await axios.get(apiUrl);
         const weatherData = response.data;
-        res.render('trails/weather', { weather: weatherData });
-    } catch (err) {
-        res.render('errors/errorPage', { errorMsg: err.message });
-    }
+        res.render('trails/weather', { weatherData });
+     } catch (err) {
+         res.render('errors/errorPage', { errorMsg: err.message });
+     }
 };
 
 
