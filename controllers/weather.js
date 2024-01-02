72afee0e10b4cef9a3b950d327968d7d9e6c3aad
HeatherRouke <heather214@gmail.com>
2024-01-02 15:09:52 -0500

    fix link and spelling

--- controllers/weather.js
+++ controllers/weather.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 
 const apiKey = 'd16785159270c1f96141537f606e54da';
 
+
 async function getWeather(req,res) {
     try {
         const apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=42.01&lon=-74.38&units=imperial&exclude=hourly,daily&appid=${apiKey}`;
