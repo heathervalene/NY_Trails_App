4e4e934cb481f3cb5a24e581c3a7951c52899faa
HeatherRouke <heather214@gmail.com>
2024-01-02 14:54:29 -0500

    add weather icon and style

--- controllers/weather.js
+++ controllers/weather.js
@@ -7,12 +7,14 @@ async function getWeather(req,res) {
         const apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=42.01&lon=-74.38&units=imperial&exclude=hourly,daily&appid=${apiKey}`;
         const response = await axios.get(apiUrl);
         const weatherData = response.data;
-        const mapTilesUrl = `https://tile.openweathermap.org/map/temp_new/3/1/1.png?appid=${apiKey}`
-        console.log(mapTilesUrl);
-        res.render('trails/weather', { weatherData, mapTilesUrl });
+        const weatherIconUrl = getWeatherIconUrl(weatherData.current.weather[0].icon)
+        res.render('trails/weather', { weatherData, weatherIconUrl});
      } catch (err) {
          res.render('errors/errorPage', { errorMsg: err.message });
      }
+     function getWeatherIconUrl() {
+        return 'https://openweathermap.org/img/w/13d.png';
+    }
 };
 
 
