79c9f20d552b43ef6793e4e0ceabd183dc52aef9
HeatherRouke <heather214@gmail.com>
2024-01-03 08:36:58 -0500

    fix weather API

--- controllers/weather.js
+++ controllers/weather.js
@@ -8,13 +8,14 @@ async function getWeather(req,res) {
         const apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=42.01&lon=-74.38&units=imperial&exclude=hourly,daily&appid=${apiKey}`;
         const response = await axios.get(apiUrl);
         const weatherData = response.data;
+        console.log(weatherData)
         const weatherIconUrl = getWeatherIconUrl(weatherData.current.weather[0].icon)
         res.render('trails/weather', { weatherData, weatherIconUrl});
      } catch (err) {
          res.render('errors/errorPage', { errorMsg: err.message });
      }
-     function getWeatherIconUrl() {
-        return 'https://openweathermap.org/img/w/13d.png';
+     function getWeatherIconUrl(iconCode) {
+        return `https://openweathermap.org/img/w/${iconCode}.png`;
     }
 };
 
